e65562885a7ec5edec28950ef64ef6b7e4c6ba67
Donald Pakkies <donaldpakkies@gmail.com>
2022-12-14 20:29:33 +0200

    feat: update Log type

--- types/Logger.d.ts
+++ types/Logger.d.ts
@@ -1,7 +1,14 @@
 import { Driver } from './Driver'
 import { LoggerInterface } from '@livy/contracts'
 
-export const Log: LoggerInterface;
+export interface Log extends LoggerInterface {
+    /**
+     * Use a different channel
+     */
+    channel: (name: string, options: any) => LoggerInterface
+}
+
+export const Log: Log;
 
 /**
  * Register a Log Driver
