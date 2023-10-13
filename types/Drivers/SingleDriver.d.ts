7618e5c56f7d2870f28ffb9a83bd65e3183638c2
Donald Pakkies <donaldpakkies@gmail.com>
2023-10-13 17:46:43 +0200

    chore: uppdate types

--- types/Drivers/SingleDriver.d.ts
+++ types/Drivers/SingleDriver.d.ts
@@ -1,7 +1,7 @@
-// auto-generated
-
-export class SingleDriver extends Driver {
-    handler(): FileHandler;
-}
 import { Driver } from "../Driver";
 import { FileHandler } from "@livy/file-handler";
+
+export class SingleDriver extends Driver {
+    get path(): string;
+    handler(): FileHandler;
+}
